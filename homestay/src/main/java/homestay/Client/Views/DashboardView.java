2723760af2272d3254b82069c936ed5c76b15f70
Huykosao <Huykosao@gmail.com>
2025-12-02 15:07:47 +0700

    Refactor view components for improved structure an d maintainability

--- homestay/src/main/java/homestay/Client/Views/DashboardView.java
+++ homestay/src/main/java/homestay/Client/Views/DashboardView.java
@@ -2,11 +2,17 @@ package homestay.Client.Views;
 
 import java.awt.Panel;
 
-public class DashboardView extends Panel {
+public class DashboardView extends Panel implements Components.IViewCheck{
 
     Panel pnlMain = new Panel();
     public void showDashboard(){
         // Thiết kế giao diện dashboard ở đây
         
+    }
+    
+    // Hàm check trước khi chuyển tab
+    @Override
+    public boolean confirmBeforeSwitch() {
+        return true; 
     }
 }
--- homestay/src/main/java/homestay/Client/Views/RoomView.java
+++ homestay/src/main/java/homestay/Client/Views/RoomView.java
@@ -12,14 +12,14 @@ import javax.swing.event.TableModelEvent;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
-public class RoomView extends Panel {
+public class RoomView extends Panel implements Components.IViewCheck{
     Panel pnlRoom = new Panel();
 
     JTable tb;
-    Button btnHuy = new Button("Huy cap nhat");
-    Button btnLuu = new Button("Luu cap nhat");
-    Button btnXoa = new Button("Xoa phong");
-    Button btnThem = new Button("Them phong");
+    Button btnHuy = new Button("Hủy thay đổi");
+    Button btnLuu = new Button("Lưu cập nhật");
+    Button btnXoa = new Button("Xóa phòng");
+    Button btnThem = new Button("Thêm phòng");
     // Biến lưu trữ dữ liệu gốc
     String originalData[][];
     public boolean isTableChanged = false;
@@ -44,10 +44,10 @@ public class RoomView extends Panel {
         gbc.gridy = 1;
 
         // Controller lấy dữ liệu từ server
-        String data[][] = { { "101", "tribeti", "Trống", "100000", "10000000" },
-                { "102", "huyenthien", "Trống", "100000", "10000000" },
-                { "101", "cter dam", "Trống", "100000", "10000000" } };
-        String column[] = { "Ma phong", "Ten Phong", "Trang Thai", "Gia thue theo ngay", "Gia thue theo thang" };
+        String data[][] = { { "tribeti", "Trống", "100000", "10000000" },
+                { "huyenthien", "Trống", "100000", "10000000" },
+                {  "cter dam", "Trống", "100000", "10000000" } };
+        String column[] = {"Ten Phong", "Trang Thai", "Gia thue theo ngay", "Gia thue theo thang" };
 
         // Lưu dữ liệu vào bộ nhớ tạm
         originalData = new String[data.length][data[0].length];
@@ -57,8 +57,23 @@ public class RoomView extends Panel {
             }
         }
 
-        // Tạo bảng
-        DefaultTableModel model = new DefaultTableModel(data, column);
+        // --- TẠO BẢNG ---
+        DefaultTableModel model = new DefaultTableModel(data, column){
+            @Override
+            public void setValueAt(Object aValue, int row, int column) {
+                // Lấy giá trị cũ đang có trong ô
+                Object oldValue = getValueAt(row, column);
+
+                // So sánh giá trị cũ và mới
+                if (oldValue != null && oldValue.toString().equals(aValue.toString())) {
+                    // Nếu giống hệt nhau
+                    return;
+                }
+                
+                // Nếu khác nhau -> Mới gọi hàm gốc để cập nhật và bắn sự kiện UPDATE
+                super.setValueAt(aValue, row, column);
+            }
+        };
         tb = new JTable(model);
         tb.setRowHeight(36);
         tb.getTableHeader().setFont(new Font("Arial", Font.BOLD, 16));
@@ -66,15 +81,7 @@ public class RoomView extends Panel {
         tb.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         JScrollPane sp = new JScrollPane(tb);
 
-        // Nếu hiện mã phòng còn không thì thôi
-        // {
-        // @Override
-        // public boolean isCellEditable(int row, int column) {
-        // // không cho sửa mã phòng
-        // return column != 0;
-        // }
-        // };
-
+    
         // --- CẤU HÌNH VALIDATION ---
         // Kiểm tra giá theo ngày và tháng
         TableColumn colNgay = tb.getColumn("Gia thue theo ngay");
@@ -82,14 +89,9 @@ public class RoomView extends Panel {
         colNgay.setCellEditor(new Components.NumericCellEditor());
         colThang.setCellEditor(new Components.NumericCellEditor());
 
-        // Giá thuê theo tháng sẽ tính theo giá thuê ngày
-        // Chưa biết
-
         // Combobox để chọn trạng thái của phòng
         String[] statusList = { "Trống", "Đang thuê", "Bảo trì" };
-
-        // Nếu không có mã phòng thì sửa lại getColumn 1
-        TableColumn statusColumn = tb.getColumnModel().getColumn(2);
+        TableColumn statusColumn = tb.getColumnModel().getColumn(1);
         JComboBox<String> comboBox = new JComboBox<>(statusList);
         statusColumn.setCellEditor(new DefaultCellEditor(comboBox));
 
@@ -101,12 +103,30 @@ public class RoomView extends Panel {
         // --- XỬ LÝ SỰ KIỆN ---
         // Nút Thêm xử lý logic thêm phòng
         btnThem.addActionListener(e -> {
+            
+            // Nếu ô tên phòng mới được để trống thì yêu cầu nhập hoặc xóa 
+            if (model.getRowCount() > 0){
+                int lastRowIndex = model.getRowCount()-1;
+                Object lastRowData = tb.getValueAt(lastRowIndex, 0);  
+                if (lastRowData.toString().isEmpty() || lastRowData == null){
+                    JOptionPane.showMessageDialog(
+                        null,
+                        "Vui lòng nhập tên phòng mới vào ô vừa thêm hoặc xóa dòng nếu không muốn thêm phòng.",
+                        "Nhắc nhở",
+                        JOptionPane.INFORMATION_MESSAGE);
+                        tb.editCellAt(lastRowIndex, 0);
+                        tb.requestFocus();
+                        return;
+                }
+            }
             isTableChanged = true;
-            String[] newRow = { "Id tang", "", "Trống", "0", "0" };
+            String[] newRow = { "", "Trống", "0", "0" };
             model.addRow(newRow);
+            
             btnHuy.setEnabled(true);
             btnLuu.setEnabled(true);
             // Controller thêm mới vào bảng
+
         });
 
         // Nút hủy Xử lý logic hủy cập nhật trả về dữ liệu ban đầu
@@ -126,17 +146,30 @@ public class RoomView extends Panel {
         // Nút Lưu Xử lý logic lưu cập nhật dữ liệu
         btnLuu.setEnabled(false);
         btnLuu.addActionListener(e -> {
+            // Dừng việc edit nếu đang gõ dở
+            if (tb.isEditing()) tb.getCellEditor().stopCellEditing();
+            
+            // Kiểm tra dữ liệu rỗng
+            for (int i = 0; i < model.getRowCount(); i++) {
+                String roomName = model.getValueAt(i, 0).toString();
+                if (roomName.trim().isEmpty() || roomName == null){
+                    JOptionPane.showMessageDialog(null, "Dòng thứ " + (i + 1) + ": Tên phòng không được để trống!");
+                    tb.setRowSelectionInterval(i, i);
+                    return;
+                }
+            }
+
             // Hộp thoại xác nhận
             int confirm = JOptionPane.showConfirmDialog(
                     null,
                     "Bạn có chắc chắn muốn cập nhật thay đổi này không?(Hãy xem kỹ lại trước khi cập nhật)",
                     "Xác nhận Cập nhật",
                     JOptionPane.YES_NO_OPTION,
-                    JOptionPane.WARNING_MESSAGE);
-
+                    JOptionPane.WARNING_MESSAGE);     
             if (confirm != JOptionPane.YES_OPTION) {
                 return;
             }
+            
             // Cập nhật lại dữ liệu original
             originalData = new String[model.getRowCount()][model.getColumnCount()];
             for (int i = 0; i < model.getRowCount(); i++) {
@@ -154,6 +187,28 @@ public class RoomView extends Panel {
 
         });
 
+        
+        // Nút Xóa
+        btnXoa.setEnabled(false);
+        btnXoa.addActionListener(e -> {
+            int selectedRow = tb.getSelectedRow();
+            if (selectedRow == -1) return;
+            
+            int confirm = JOptionPane.showConfirmDialog(
+                null,
+                "Bạn có chắc chắn muốn xóa dòng này không?",
+                "Xác nhận xóa",
+                JOptionPane.YES_NO_OPTION);
+                if (confirm == JOptionPane.YES_OPTION) {
+                    model.removeRow(selectedRow);
+                    isTableChanged = true;
+                    btnHuy.setEnabled(true);
+                    btnLuu.setEnabled(true);
+                    
+                }
+                
+            });
+            
         // Sự kiện chọn dòng để bật nút Xóa
         tb.getSelectionModel().addListSelectionListener(e -> {
             if (!e.getValueIsAdjusting()) {
@@ -167,44 +222,16 @@ public class RoomView extends Panel {
                 }
             }
         });
-
-        // Nút Xóa
-        btnXoa.setEnabled(false);
-        btnXoa.addActionListener(e -> {
-            int selectedRow = tb.getSelectedRow();
-            if (selectedRow == -1) return;
-            
-            int confirm = JOptionPane.showConfirmDialog(
-                    null,
-                    "Bạn có chắc chắn muốn xóa " + selectedRow + " dòng không?",
-                    "Xác nhận xóa",
-                    JOptionPane.YES_NO_OPTION);
-            if (confirm == JOptionPane.YES_OPTION) {
-                model.removeRow(selectedRow);
-                isTableChanged = true;
-                btnHuy.setEnabled(true);
-                btnLuu.setEnabled(true);
-            }
-            
-        });
-
-        // Lấy dữ liệu khi thay đổi trong bảng
+        
+        // Lắng nghe thay đổi trên ô dữ liệu
         tb.getModel().addTableModelListener(e -> {
             if (e.getType() == TableModelEvent.UPDATE) {
                 isTableChanged = true;
                 btnHuy.setEnabled(true);
                 btnLuu.setEnabled(true);
-                // Nếu không cần có thể xóa !!!
-                int row = e.getFirstRow();
-                int columnValue = e.getColumn();
-                // Phần này để lấy dữ liệu của ô đó
-                Object data1 = tb.getValueAt(row, columnValue);
-                // Phần này để lấy tên cột
-                String columnName = tb.getColumnName(columnValue);
-
             }
         });
-
+        // Thêm nút vào panel
         pnlButtons.add(btnThem);
         pnlButtons.add(btnXoa);
         pnlButtons.add(btnHuy);
@@ -213,17 +240,18 @@ public class RoomView extends Panel {
 
     }
 
-    // Hàm cảnh báo trước khi chuyển trang 
+    // Hàm cảnh báo trước khi chuyển trang
+    @Override 
     public boolean confirmBeforeSwitch() {
-        // Nếu chưa thay đổi thì chuyển bình thường
+        
         if (!isTableChanged) return true;
 
         JOptionPane.showMessageDialog(
                 null,
-                "Dữ liệu đã thay đổi! Hãy cập nhật hoặc hủy thay đổi trước khi rời khỏi.",
+                "Dữ liệu trong bảng đã thay đổi! Hãy cập nhật hoặc hủy thay đổi trước khi rời khỏi.",
                 "Cảnh báo",
                 JOptionPane.WARNING_MESSAGE);
         return false;
     }
-    // Xu ly nut them llai!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
 }
